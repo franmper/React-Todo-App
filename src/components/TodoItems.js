5f3d707d0609b6e185e65e9fa154bb2a0307ddf5
franmper <38388588+franmper@users.noreply.github.com>
2019-12-05 10:36:30 -0300

    Create TodoItems.js

---
+++ src/components/TodoItems.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+class TodoItems extends Component {
+    createTask = item => {
+        return <li key={item.key} className="item" onClick={() => this.props.deleteItem(item.key)}>{item.text}</li>
+    }
+    render() {
+            const todoEntries = this.props.entries
+            const listItems = todoEntries.map(this.createTask)
+
+          return <ul className="theList">{listItems}</ul>
+        
+    }
+}
+
+export default TodoItems
