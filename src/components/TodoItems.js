c1e1c93adfe26b884a7ef8a0c16c4797859d9df8
franmper <38388588+franmper@users.noreply.github.com>
2019-12-13 10:31:44 -0300

    commit on master

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -2,7 +2,7 @@ import React, {Component} from "react"
 
 class TodoItems extends Component {
     createTask = item => {
-        return <li key={item.key} className="item" onClick={() => this.props.deleteItem(item.key)}>{item.text}</li>
+        return <li key={item.key} className="item"><i class="fas fa-times"  onClick={() => this.props.deleteItem(item.key)}></i>{item.text}</li>
     }
     render() {
             const todoEntries = this.props.entries
